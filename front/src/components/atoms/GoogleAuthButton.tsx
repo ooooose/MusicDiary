93083c639b07732cafa031f17d8d14c6bc97521c
ooooose <yuuki.oose1990@gmail.com>
2024-06-13 09:35:12 +0900

    Add 認証ボタンの作成

--- front/src/components/atoms/GoogleAuthButton.tsx
+++ front/src/components/atoms/GoogleAuthButton.tsx
@@ -1,7 +1,9 @@
-
+import { Button } from '@/components/ui/button'
+import { signIn } from 'next-auth/react'
 
 export const GoogleAuthButton = () => {
-  return (
-    <>ボタン</>
-  )
+  const handleSignIn = () => {
+    signIn('google')
+  }
+  return <Button variant="outline" onClick={handleSignIn}>認証ボタン</Button>
 }
---
+++ front/src/features/diaries/types/index.ts
@@ -0,0 +1,5 @@
+export type Diary = {
+  uid: string
+  body: string
+  createdAt: string
+}
