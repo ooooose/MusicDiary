b382032a62f190d40dd8495fdf5402a09fb38c94
ooooose <yuuki.oose1990@gmail.com>
2024-06-18 09:49:43 +0900

    Add JWTトークンの検証処理を実装

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate
 
-  before_action :authenticate_user!
+  def encode_jwt(payload)
+    JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS256')
+  end
+
+  attr_reader :current_user
 
   private
 
-  def authenticate_user!
-    unless current_user
-      render json: { error: 'Unauthorized' }, status: :unauthorized
-    end
-  end
+  def authenticate
+    encoded_token = request.headers['Authorization']&.split&.last
+    @current_user = User.find_with_jwt(encoded_token) if encoded_token
 
+    return if @current_user
+
+    render json: { error: '認証に失敗しました' }, status: :unauthorized
+  end
 end
