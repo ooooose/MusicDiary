f93336fce4f48cc384d71ecd5818dde228c904c6
ooooose <yuuki.oose1990@gmail.com>
2024-06-18 09:26:00 +0900

    Fix データベースの変更

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
-
-  include DeviseTokenAuth::Concerns::User
-
   has_many :diaries, dependent: :destroy
 
   ROLES = { general: 1, admin: 9 }.freeze
@@ -23,15 +17,6 @@ class User < ApplicationRecord
   before_validation :set_default_role, on: :create
   before_create :set_uid
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
-      user.image = auth.info.image
-    end
-  end
-
   private
 
     def set_default_role
@@ -41,5 +26,4 @@ class User < ApplicationRecord
     def set_uid
       self.uid = SecureRandom.uuid
     end
-
 end
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_17_001447) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_18_002319) do
   create_table "diaries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "uid", null: false
     t.text "body", null: false
@@ -22,32 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_17_001447) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "name"
-    t.string "nickname"
+    t.string "name", limit: 40, null: false
+    t.string "uid", null: false
+    t.string "email", null: false
     t.string "image"
-    t.string "email"
     t.integer "role", default: 1, null: false
     t.boolean "deleted_flag", default: false, null: false
-    t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["deleted_flag"], name: "index_users_on_deleted_flag"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.index ["email"], name: "unique_emails", unique: true
+    t.index ["uid"], name: "unique_uid", unique: true
   end
 
   add_foreign_key "diaries", "users"
