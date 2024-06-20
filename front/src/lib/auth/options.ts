1c3635b8140774297c8cbaba15a0264ca45c5e88
ooooose <yuuki.oose1990@gmail.com>
2024-06-20 09:41:42 +0900

    Fix 404エラー修正中

--- front/src/lib/auth/options.ts
+++ front/src/lib/auth/options.ts
@@ -1,6 +1,8 @@
 import type { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
 export const options: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
@@ -13,40 +15,44 @@ export const options: NextAuthOptions = {
     strategy: 'jwt',
     maxAge: 7 * 24 * 60 * 60,
   },
-  pages: {
-    signIn: '/login',
-  },
   callbacks: {
     async signIn({ user, account }) {
-      const provider = account?.provider
-      const name = user?.name
-      const email = user?.email
+      const provider = account?.provider;
+      const name = user?.name;
+      const email = user?.email;
+
+      if (!provider || !name || !email) {
+        console.error('認証情報の取得に失敗しました');
+        return false;
+      }
+
       try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/auth/${provider}/callback`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              user: {
-                name,
-                email,
-              },
-            }),
+        const response = await fetch(`${apiUrl}/auth/${provider}/callback`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
           },
-        )
-        if (response.status === 200) {
-          const data = await response.json()
-          user.userId = data.user.id
-          user.accessToken = data.accessToken
-          return true
+          body: JSON.stringify({
+            user: {
+              name,
+              email,
+            },
+          }),
+        })
+
+        console.log(`Response status: ${response.status}`);
+        if (response.ok) {
+          const data = await response.json();
+          user.userId = data.user.id;
+          user.accessToken = data.accessToken;
+          return true;
         } else {
-          return false
+          console.error(`Error: ${response.status} ${response.statusText}`);
+          return false;
         }
       } catch (error) {
-        return false
+        console.error(`Fetch error: ${error}`);
+        return false;
       }
     },
     async redirect({ baseUrl }) {
