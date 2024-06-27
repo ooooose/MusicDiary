c44a05165f0cfc3e3f909544719b219302a97267
ooooose <yuuki.oose1990@gmail.com>
2024-06-27 09:21:03 +0900

    Add 新規作成ボタンの作成

--- front/src/features/diaries/components/diaries-calendar.tsx
+++ front/src/features/diaries/components/diaries-calendar.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import Link from 'next/link'
 import { useState } from 'react'
 import { useDiaries } from '@/features/diaries/api'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Calendar } from '@/components/ui/calendar'
+import { Button } from '@/components/ui/button'
 
 export const DiariesCalendar = () => {
   const [date, setDate] = useState<Date | undefined>(new Date())
@@ -19,6 +21,12 @@ export const DiariesCalendar = () => {
         diaries={diariesQuery.data ?? []}
         className="rounded-md border shadow"
       />
+
+      <Button variant='outline' className='mt-4 w-full'>
+        <Link href='/diaries'>
+          日記を作成する
+        </Link>
+      </Button>
     </div>
   )
 }
