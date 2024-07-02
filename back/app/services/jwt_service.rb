7150595b6de879d748e125fcfc81247c64facb22
ooooose <yuuki.oose1990@gmail.com>
2024-07-02 15:49:28 +0900

    Fix 設定していたexpを削除

--- back/app/services/jwt_service.rb
+++ back/app/services/jwt_service.rb
@@ -1,8 +1,7 @@
 class JwtService
   SECRET_KEY = Rails.application.credentials.secret_key_base
 
-  def self.encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
+  def self.encode(payload)
     JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
