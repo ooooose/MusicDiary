e5b49cff1e79aeb721ecdd14039a121e5f856164
ooooose <yuuki.oose1990@gmail.com>
2024-07-08 06:49:24 +0900

    Fix 命名の整理

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 
-import NextAuthProvider from '@/lib/auth/NextAuthProvider'
+import NextAuthProvider from '@/lib/auth/next-auth-provider'
 import { cn } from '@/lib/utils'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
