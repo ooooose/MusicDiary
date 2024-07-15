0ebbcdc27e69cb464ef39c7b8e3baac749e99c90
ooooose <yuuki.oose1990@gmail.com>
2024-07-15 16:46:17 +0900

    Add テスト記述追加

--- back/spec/requests/diaries_spec.rb
+++ back/spec/requests/diaries_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe "Diaries", type: :request do
   let!(:headers) { { Authorization: "Bearer #{token}" } }
 
   describe 'GET /api/v1/diaries' do
-    context 'when the user does not have any diaries' do
-      it 'returns nil' do
-        get(api_v1_diaries_path, headers:)
+    context 'when the user does not have any diaries' do it 'returns nil' do get(api_v1_diaries_path, headers:)
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)['data'].length).to eq(0)
       end
@@ -17,7 +15,6 @@ RSpec.describe "Diaries", type: :request do
     context 'when the user has diaries' do
       it 'returns diaries' do
         create_list(:diary, 3, user:)
-        binding.pry
 
         get(api_v1_diaries_path, headers:)
         expect(response).to have_http_status(:ok)
@@ -32,4 +29,27 @@ RSpec.describe "Diaries", type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/diaries' do
+    context 'when authenticated' do
+      context 'when params are valid' do
+        it 'creates an diary' do
+          post(api_v1_diaries_path, params: { diary: { body: 'test', uid: user.uid } }, headers:)
+          expect(response).to have_http_status(:created)
+
+          expect(JSON.parse(response.body)['body']).to eq('test')
+          expect(JSON.parse(response.body)['user_id']).to eq(user.id)
+        end
+      end
+
+      context 'when params are invalid' do
+        it 'returns errors' do
+          post(api_v1_diaries_path, params: { diary: { body: nil, uid: user.uid } }, headers:)
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+  end
+
+
 end
