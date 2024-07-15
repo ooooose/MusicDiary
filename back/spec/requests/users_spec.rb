12dbf219302a4010b3d00cf1e4fdd22c5b7765b7
ooooose <yuuki.oose1990@gmail.com>
2024-07-15 18:48:11 +0900

    Add テスト追記

--- back/spec/requests/users_spec.rb
+++ back/spec/requests/users_spec.rb
@@ -1,10 +1,27 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response).to have_http_status(200)
+  let!(:user) { create(:user) }
+  let!(:token) { encode_jwt({ user_id: user.id, exp: 24.hours.from_now.to_i }) }
+  let!(:headers) { { Authorization: "Bearer #{token}" } }
+
+  describe 'POST /auth/google/callback' do
+    context 'when valid request' do
+      it 'creates a new user and returns accessToken' do
+        user_params = attributes_for(:user)
+        post '/auth/google/callback', params: { user: user_params }
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['user']['email']).to eq(user_params[:email])
+        expect(JSON.parse(response.body)['accessToken']).to be_present
+      end
+    end
+
+    context 'when invalid request' do
+      it 'returns internal_server_error status' do
+        post '/auth/google/callback', params: { user: { email: '', name: '' } }
+        expect(response).to have_http_status(:internal_server_error)
+        expect(JSON.parse(response.body)['error']).to include('ログインに失敗しました')
+      end
     end
   end
 end
