bb652cba19e4c77b87fa41f39b9a09299ebf0b93
ooooose <yuuki.oose1990@gmail.com>
2024-07-15 18:10:01 +0900

    make rubocop

--- back/spec/models/diary_spec.rb
+++ back/spec/models/diary_spec.rb
@@ -1,25 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Diary, type: :model do
   # validationのテスト
-  describe 'validation' do
-    context 'when normal' do
+  describe "validation" do
+    context "when normal" do
       # ユーザーIDと日記の内容があれば、有効であること
-      it 'is valid with a name and email' do
+      it "is valid with a name and email" do
         diary = build(:diary)
         expect(diary).to be_valid
       end
     end
 
-    context 'when abnormal' do
+    context "when abnormal" do
       # ユーザーIDがなければ、無効であること
-      it 'is invalid without user' do
+      it "is invalid without user" do
         diary = build(:diary, user_id: nil)
         expect(diary).not_to be_valid
       end
 
       # 日記の内容がなければ、無効であること
-      it 'is invalid without body' do
+      it "is invalid without body" do
         diary = build(:diary, body: nil)
         expect(diary).not_to be_valid
       end
@@ -27,7 +27,7 @@ RSpec.describe Diary, type: :model do
   end
 
   # associationのテスト
-  describe 'association' do
+  describe "association" do
     it { is_expected.to have_many(:tracks).dependent(:destroy) }
   end
 end
--- back/spec/models/track_spec.rb
+++ back/spec/models/track_spec.rb
@@ -1,42 +1,42 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Track, type: :model do
   # validationのテスト
-  describe 'validation' do
-    context 'when normal' do
-      it 'is valid with a name and email' do
+  describe "validation" do
+    context "when normal" do
+      it "is valid with a name and email" do
         track = build(:track)
         expect(track).to be_valid
       end
     end
 
-    context 'when abnormal' do
+    context "when abnormal" do
       # タイトルがなければ、無効であること
-      it 'is invalid without title' do
+      it "is invalid without title" do
         track = build(:track, title: nil)
         expect(track).not_to be_valid
       end
-      
+
       # アーティストがなければ、無効であること
-      it 'is invalid without artist' do
+      it "is invalid without artist" do
         track = build(:track, artist: nil)
         expect(track).not_to be_valid
       end
 
       # SpotifyIDがなければ、無効であること
-      it 'is invalid without spotifyID' do
+      it "is invalid without spotifyID" do
         track = build(:track, spotify_id: nil)
         expect(track).not_to be_valid
       end
 
       # イメージがなければ、無効であること
-      it 'is invalid without image' do
+      it "is invalid without image" do
         track = build(:track, image: nil)
         expect(track).not_to be_valid
       end
 
       # diaryIDがなければ、無効であること
-      it 'is invalid without diaryID' do
+      it "is invalid without diaryID" do
         track = build(:track, diary_id: nil)
         expect(track).not_to be_valid
       end
