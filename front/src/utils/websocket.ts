a605c362be80386aff3fa34f7a920a9f89758f13
ooooose <yuuki.oose1990@gmail.com>
2024-07-24 22:45:36 +0900

    Fix SSR対策

--- front/src/utils/websocket.ts
+++ front/src/utils/websocket.ts
@@ -1,4 +1,10 @@
 export const getWebSocketUrl = () => {
+  if (typeof window === 'undefined') {
+    const defaultProtocol = 'wss:'
+    const defaultHost = process.env.NEXT_PUBLIC_API_URL || 'localhost'
+    return `${defaultProtocol}//${defaultHost}/cable`
+  }
+
   const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
   const host = process.env.NEXT_PUBLIC_API_URL || window.location.host
   return `${protocol}//${host}/cable`
